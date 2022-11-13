5c5a105ce2a810dcae019e1596697129cdbcce5f
Diana Chistyakova <103204655+Shirayuky@users.noreply.github.com>
2022-11-13 18:32:09 +0300

    Create Loggers

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, request, render_template, send_from_directory
 from loader.views import loader_blueprint
 from main.views import main_blueprint
+import logging
+
+# logger = logging.basicConfig(
+#     filename='flask.log',
+#     level=logging.DEBUG
+# )
 
 POST_PATH = "posts.json"
 UPLOAD_FOLDER = "uploads/images"
